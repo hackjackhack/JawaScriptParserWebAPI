f75ad5082b8cd4ec560d9915c65690485c42306b
Chi-Wei Wang <jackwang@intowow.com>
2016-01-05 14:27:27 +0800

    Initial commit

---
+++ src/WebService.java
@@ -0,0 +1,31 @@
+import static spark.Spark.*;
+
+import main.org.jackace.jawascriptparser.AST;
+import main.org.jackace.jawascriptparser.Parser;
+import main.org.jackace.jawascriptparser.Token;
+import main.org.jackace.jawascriptparser.Tokenizer;
+import main.org.jackace.jawascriptparser.Serializer;
+import main.org.jackace.jawascriptparser.UnexpectedTokenException;
+
+import java.util.ArrayList;
+
+/**
+ * Created by jackace on 2016/1/5.
+ */
+public class WebService {
+    public static void main(String[] args) {
+        post("/parse", (request, response) -> {
+            Tokenizer tn = new Tokenizer();
+            String program = request.body();
+            try {
+                ArrayList<Token> tokens = tn.tokenize(program);
+                Parser ps = new Parser();
+                AST tree = ps.parse(tokens);
+                String output = Serializer.toJSON(tree);
+                return output;
+            } catch (UnexpectedTokenException e) {
+                return "";
+            }
+        });
+    }
+}
